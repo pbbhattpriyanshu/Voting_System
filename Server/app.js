f71b456918efd8159c2ff5dffb928241e798cdf8
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-12-15 18:57:59 +0530

    feat(server): add CORS and cookie-parser middleware

--- Server/app.js
+++ Server/app.js
@@ -1,5 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //route
 app.get('/', (req, res) => {
