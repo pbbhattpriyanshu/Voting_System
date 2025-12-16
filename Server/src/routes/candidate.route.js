4d96cde00fd964477a7bde4cd8590d0b3bcf52be
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-12-16 21:30:29 +0530

    `Updated candidate route with new controller imports and route methods`

--- Server/src/routes/candidate.route.js
+++ Server/src/routes/candidate.route.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { } from '../controllers/auth.controller.js';
+import {registerCandidate, updateCandidate, deleteCandidate } from '../controllers/candidate.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 const router = Router();
 
 
-router.post('/register')
-router.post('/update/:id' )
-router.post('/delete/:id' )
+router.post('/register', protectRoute, registerCandidate);
+router.put('/update/:id', protectRoute, updateCandidate);
+router.delete('/delete/:id', protectRoute, deleteCandidate);
 
 
 
