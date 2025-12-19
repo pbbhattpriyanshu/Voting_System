0586387bf44282b491391ead299875c9825b9e1a
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-12-19 20:31:43 +0530

    `Added voting functionality to voteToCandidate controller`

--- Server/src/controllers/voting.controllers.js
+++ Server/src/controllers/voting.controllers.js
@@ -10,8 +10,39 @@ export const voteToCandidate = async (req, res) => {
 
     try {
         //find candidate by id
-        
+        const candidate = await Candidate.findById(candidateId);
+        if(!candidate){
+            return res.status(404).json({message: 'Candidate not found'});
+        }
+
+        //find user id
+        const user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({message: 'User not found'});
+        }
+
+        //User is already voted
+        if(user.isVoted){
+            return res.status(400).json({message: 'You have already voted'})
+        }
+
+        //User is admin
+        if(user.role == 'admin'){
+            return res.status(403).json({message: "Admin can't be vote"})
+        }
+
+        //Update the candidate document
+        candidate.votes.push({user: userId});
+        candidate.voteCount++;  
+        await candidate.save();
+
+        //Update the user documents
+        user.isVoted  = true;
+        await user.save();
+
+        return res.status(200).json({sucess: true, message: 'Vote recorded Sucessfully'})
+    } catch(error){
+        console.error("Error in deleteCandidate controller", error);
+        res.status(500).json({ message: "Server error" });
     }
-
-
 }
